52103a23f879672e9ba4a1339960e29d45d09568
Yuri Tiófilo Silva <yusilva12345@gmail.com>
2022-02-03 00:37:48 -0300

    feat: method sign in implementation

--- src/components/Forms/SignInForm/index.tsx
+++ src/components/Forms/SignInForm/index.tsx
@@ -1,23 +1,36 @@
 import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
 
+import auth from '@react-native-firebase/auth';
+
 import { FooterButton } from "@components/Controllers/FooterButton";
 import { Button } from "@components/Controllers/Button";
 import { Input } from "@components/Controllers/Input";
 import { Form, Title, Footer } from "./styles";
+import { Alert } from "react-native";
 
 export function SignInForm() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const navigation = useNavigation();
 
   function handleSignIn() {
     setIsLoading(true);
+
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        Alert.alert('Login', "Bem vindo")
+        setIsLoading(false)
+      })
+      .catch((error) => console.log('deu ruim', error))
+      .finally()
   }
 
-  function handleForgotPassword() {}
+  function handleForgotPassword() {
+  }
 
   return (
     <Form>
